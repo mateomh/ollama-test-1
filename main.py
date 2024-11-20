9a1d2d7fe1331025e26a86ffa977e526cd2d9017
Mateo mojica <mmojicah.84@gmail.com>
2024-11-19 21:03:37 -0500

    basic usage of a template and chain

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 
 model = OllamaLLM(model="llama3")
 
-result = model.invoke(input="hello world")
+# result = model.invoke(input="hello world")
+
+# print(result)
+
+template = """
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt | model
+
+result = chain.invoke({"context": "", "question": "how are you?"})
 
 print(result)
